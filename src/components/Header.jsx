68e0b91e23acae4f019898ff4f2e4b400fa2b782
Ramil3107 <106266508+Ramil3107@users.noreply.github.com>
2022-06-20 19:01:02 +0300

    Add grids, create app structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://w7.pngwing.com/pngs/786/126/png-transparent-logo-contracting-photography-logo-symbol.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src='https://images.ctfassets.net/hrltx12pl8hq/69FDo9oeiebEZtaKX7Iq2V/d55fe9c40ef50e33912a025d2ff51b19/cms-bg-img.jpg?fit=fill&w=840&h=150' />
+            </div>
+            <div>avatar + dscrp</div>
+            <div>My posts
+                <div>New post</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
