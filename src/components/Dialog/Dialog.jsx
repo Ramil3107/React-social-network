a28fb5db1db3550cc58262bca321a22ddd598d75
Ramil3107 <106266508+Ramil3107@users.noreply.github.com>
2022-06-23 19:32:16 +0300

    Add dialogs block

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialog.module.css";
 
 const Dialog = () => {
     return (
-        <div>
-            Dialog
+        <div className={s.dialogs}>
+            <div className={s.dialogUsers}>
+                <div className={s.users + " " + s.active}>
+                    <NavLink to="/dialog/1">Dmitry</NavLink>
+                </div>
+                <div className={s.users}>
+                    <NavLink to="/dialog/2">Sasha</NavLink>
+                </div>
+                <div className={s.users}>
+                    <NavLink to="/dialog/3"> Alex</NavLink>
+                </div>
+                <div className={s.users}>
+                    <NavLink to="/dialog/4">Pavel</NavLink>
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi man!
+                </div>
+                <div className={s.message}>
+                    How r u?
+                </div>
+                <div className={s.message}>
+                    My name is Ramil
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import s from "./Navigation.module.css"
 
-const Navigation = () => {
+const Navigation = (props) => {
     return (<nav className={s.nav}>
-        <div className={s.item}><a href="/profile">Profile</a></div>
-        <div><a href="/dialog">Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
+        <div className={s.item}>
+            <NavLink to="/profile" className={({ isActive }) => (isActive ? [s.active] : [])}>Profile</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/dialog" className={({ isActive }) => (isActive ? [s.active] : [])}>Messages</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/news" className={({ isActive }) => (isActive ? [s.active] : [])}>News</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/music" className={({ isActive }) => (isActive ? [s.active] : [])}>Music</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="settings" className={({ isActive }) => (isActive ? [s.active] : [])}>Settings</NavLink>
+        </div>
     </nav>
     )
 }
