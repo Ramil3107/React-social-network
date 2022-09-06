dda305d7d5edd8273e66ba07ed12e79b76ccd043
Ramil3107 <abbasov.ram3107@gmail.com>
2022-09-06 16:34:51 +0300

    react lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import { Route, Routes, } from 'react-router-dom';
-import DialogContainer from './components/Dialog/DialogContainer';
-import FindUsersContainer from './components/FindUsers/FindUsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/appReducer';
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
+import Preloader from './components/common/Preloader/Preloader';
+
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogContainer = lazy(() => import('./components/Dialog/DialogContainer'));
+const FindUsersContainer = lazy(() => import('./components/FindUsers/FindUsersContainer'));
 
 
 
@@ -36,18 +38,21 @@ const App = (props) => {
           </div>
           {
             props.initialized ? (<div id='content' className='item'>
-              <Routes>
+              <Suspense fallback={<Preloader />}>
+                <Routes>
 
-                <Route path='/profile/' element={<ProfileContainer />} >
-                  <Route path=':userId' element={<ProfileContainer />} />
-                </Route>
+                  <Route path='/profile/' element={<ProfileContainer />} >
+                    <Route path=':userId' element={<ProfileContainer />} />
+                  </Route>
 
-                <Route path="/dialog/" element={<DialogContainer />} />
+                  <Route path="/dialog/" element={<DialogContainer />} />
 
-                <Route path="/users" element={<FindUsersContainer />} />
-                <Route path="/login" element={<Login />} />
+                  <Route path="/users" element={<FindUsersContainer />} />
 
-              </Routes>
+                  <Route path="/login" element={<Login />} />
+
+                </Routes>
+              </Suspense>
             </div>)
               :
               (<Login />)
