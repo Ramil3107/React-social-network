10391e6d5a35ab156ab6bb52fc85300768fc7593
Ramil3107 <abbasov.ram3107@gmail.com>
2022-09-06 16:34:29 +0300

    pagination

--- src/components/FindUsers/Pagination.jsx
+++ src/components/FindUsers/Pagination.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react"
 import s from "./FindUsers.module.css"
 
 
 
-const Pagination = ({ usersTotalCount, pageUsersLimit, onPageChanged, currentPage }) => {
+const Pagination = ({ usersTotalCount, pageUsersLimit, onPageChanged, currentPage, portionSize = 20 }) => {
 
     let pagesCount = Math.ceil(usersTotalCount / pageUsersLimit)
+    console.log(pagesCount)
     let pages = []
-    for (let i = 1; i <= pagesCount && i <= 30; i++) {
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
 
+
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    let [portionNumber, setPortionNumber] = useState(1)
+    let portionFirstPageNumber = (portionNumber - 1) * portionSize + 1
+    let portionLastPageNumber = portionNumber * portionSize
+
+
     return (
-        <div className={s.pages}>
-            {
-                pages.map(p => <span
-                    key={p}
-                    onClick={() => onPageChanged(p)}
-                    className={currentPage === p ? s.selectedPage : s.page}>
-                    {p}
-                </span>)
-            }
+        <div className={s.paginationWrapper}>
+            <button disabled={portionNumber <= 1} onClick={() => setPortionNumber(portionNumber - 1)} >Prev</button>
+            <span className={s.pages}>
+                {
+                    pages
+                        .filter(p => p >= portionFirstPageNumber && p <= portionLastPageNumber)
+                        .map(p => {
+                            return (
+                                <span
+                                    key={p}
+                                    onClick={() => onPageChanged(p)}
+                                    className={currentPage === p ? s.selectedPage : s.page}
+                                >
+                                    {p}
+                                </span>
+                            )
+                        })
+                }
+            </span>
+            <button disabled={portionNumber > portionCount} onClick={() => setPortionNumber(portionNumber + 1)} >Next</button>
         </div>
     )
 }
